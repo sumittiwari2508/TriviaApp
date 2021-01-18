c047b27e653dcafefb503bda0bbb3df21e881ec9
sumittiwari2508 <sumit.k@bizfab.com>
2021-01-18 06:19:36 -0500

    Initial Commit

---
+++ TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Kasun on 10/29/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TriviaAppTests/TriviaAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  TriviaAppTests.swift
+//  TriviaAppTests
+//
+//  Created by Kasun on 10/29/1399 AP.
+//
+
+import XCTest
+@testable import TriviaApp
+
+class TriviaAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
